73f189aec718df033e5c9d8c8e375e132b24403a
mahapatrak898 <mahapatrak898.kkm@gmail.com>
2024-04-08 18:21:03 +0530

    v3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,109 +14,133 @@ import {
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Spinner } from "@chakra-ui/react";
 
 export default function Login() {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigate();
 
   async function HandleLogin(event) {
     event.preventDefault(); // Prevents the default form submission behavior
+    setLoading(true)
     axios
       .post(`https://kryzen-backend-p0vu.onrender.com/login`, {
         email: Email,
         password: Password,
       })
       .then((response) => {
-        console.log(response)
-        if (response.data === "Wrong credentials" || response.data === "email does not exist, please signup first") {
-          window.alert("Login failed. Please check your credentials and try again.");
+        setLoading(false)
+        console.log(response);
+        if (
+          response.data === "Wrong credentials" ||
+          response.data === "email does not exist, please signup first"
+        ) {
+          window.alert(
+            "Login failed. Please check your credentials and try again."
+          );
         } else {
           const token = response.data.token;
           localStorage.setItem("token", token);
           navigation("/dashboard");
           window.alert("Login successful!");
-          window.location.reload()
+          window.location.reload();
         }
       })
       .catch((err) => {
-          console.log("Login failed:", err.response.data.error);
-          // You can handle other errors here, such as displaying an error message to the user
+        setLoading(false)
+        console.log("Login failed:", err.response.data.error);
+        // You can handle other errors here, such as displaying an error message to the user
       });
-  }  
+  }
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            to enjoy all of our cool <Text color={"blue.400"}>features</Text> ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <form onSubmit={HandleLogin}>
-            <Stack spacing={4}>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input
-                  type="email"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Password</FormLabel>
-                <Input
-                  type="password"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: "column", sm: "row" }}
-                  align={"start"}
-                  justify={"space-between"}
-                >
-                  <Checkbox>Remember me</Checkbox>
-                  <Text
-                    cursor={"grab"}
-                    onClick={() => {
-                      navigation("/signup");
-                    }}
-                    color={"blue.400"}
-                  >
-                    Sign Up
-                  </Text>
+    <>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              to enjoy all of our cool <Text color={"blue.400"}>features</Text>{" "}
+              ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            {loading && (
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="xl"
+              />
+            )}
+            {!loading && (
+              <form onSubmit={HandleLogin}>
+                <Stack spacing={4}>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input
+                      type="email"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </FormControl>
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      type="password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </FormControl>
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: "column", sm: "row" }}
+                      align={"start"}
+                      justify={"space-between"}
+                    >
+                      <Checkbox>Remember me</Checkbox>
+                      <Text
+                        cursor={"grab"}
+                        onClick={() => {
+                          navigation("/signup");
+                        }}
+                        color={"blue.400"}
+                      >
+                        Sign Up
+                      </Text>
+                    </Stack>
+                    <Button
+                      type="submit" // Ensures the button triggers form submission
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                    >
+                      Sign in
+                    </Button>
+                  </Stack>
                 </Stack>
-                <Button
-                  type="submit" // Ensures the button triggers form submission
-                  bg={"blue.400"}
-                  color={"white"}
-                  _hover={{
-                    bg: "blue.500",
-                  }}
-                >
-                  Sign in
-                </Button>
-              </Stack>
-            </Stack>
-          </form>
-        </Box>
-      </Stack>
-    </Flex>
+              </form>
+            )}
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -19,6 +19,7 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Spinner } from "@chakra-ui/react";
 
 export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
@@ -27,28 +28,39 @@ export default function Signup() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigate();
 
   async function HandleSignup(event) {
     event.preventDefault(); // Prevents the default form submission behavior
+    setLoading(true);
     try {
-      const response = await axios.post(`https://kryzen-backend-p0vu.onrender.com/signup`, {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-      });
+      const response = await axios.post(
+        `https://kryzen-backend-p0vu.onrender.com/signup`,
+        {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+        }
+      );
       // Assuming your backend returns a message indicating the email already exists
       const data = response.data;
       console.log(data);
-      if (data === "This Email already exists, please login or use a different email") {
+      if (
+        data ===
+        "This Email already exists, please login or use a different email"
+      ) {
+        setLoading(false)
         window.alert(data);
       } else {
+        setLoading(false)
         window.alert("Signup successful!");
         navigation("/login");
       }
     } catch (err) {
+      setLoading(false)
       console.log("Signup failed:", err.response.data.error);
       // You can handle other errors here, such as displaying an error message to the user
     }
@@ -76,91 +88,102 @@ export default function Signup() {
           boxShadow={"lg"}
           p={8}
         >
-          <form onSubmit={HandleSignup}>
-          <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  <FormLabel>First Name</FormLabel>
+          {loading && (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          )}
+          {!loading && (
+            <form onSubmit={HandleSignup}>
+              <Stack spacing={4}>
+                <HStack>
+                  <Box>
+                    <FormControl id="firstName" isRequired>
+                      <FormLabel>First Name</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => {
+                          setFirstName(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                  </Box>
+                  <Box>
+                    <FormControl id="lastName" isRequired>
+                      <FormLabel>Last Name</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={(e) => {
+                          setLastName(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
                   <Input
-                    type="text"
+                    type="email"
                     onChange={(e) => {
-                      setFirstName(e.target.value);
+                      setEmail(e.target.value);
                     }}
                   />
                 </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName" isRequired>
-                  <FormLabel>Last Name</FormLabel>
-                  <Input
-                    type="text"
-                    onChange={(e) => {
-                      setLastName(e.target.value);
-                    }}
-                  />
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                    <InputRightElement h={"full"}>
+                      <Button
+                        variant={"ghost"}
+                        onClick={() =>
+                          setShowPassword((showPassword) => !showPassword)
+                        }
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
                 </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input
-                type="email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-                <InputRightElement h={"full"}>
+                <Stack spacing={10} pt={2}>
                   <Button
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
+                    type="submit"
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={"blue.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}
                   >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    Sign up
                   </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                type="submit"
-                loadingText="Submitting"
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
-                Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user?{" "}
-                <Link
-                  onClick={() => {
-                    navigation("/login");
-                  }}
-                  color={"blue.400"}
-                >
-                  Login
-                </Link>
-              </Text>
-            </Stack>
-          </Stack>
-          </form>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align={"center"}>
+                    Already a user?{" "}
+                    <Link
+                      onClick={() => {
+                        navigation("/login");
+                      }}
+                      color={"blue.400"}
+                    >
+                      Login
+                    </Link>
+                  </Text>
+                </Stack>
+              </Stack>
+            </form>
+          )}
         </Box>
       </Stack>
     </Flex>
